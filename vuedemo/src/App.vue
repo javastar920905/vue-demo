d95aaa26b813b9d1266bc3120b45b8fbdedb445a
ouzx@hua-cloud.com.cn <ouzx@hua-cloud.com.cn>
2018-12-20 18:42:55 +0800

    vue 照片分享demo

--- vuedemo/src/App.vue
+++ vuedemo/src/App.vue
@@ -1,23 +1,59 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="com-app">
+   <com-header></com-header> 
+     <transition :name="transitionName">
+      <router-view class="child-view"></router-view>
+    </transition> 
+    <!-- <com-addMsg></com-addMsg> -->
+    <com-footer v-show="isFooter"></com-footer> 
+    <com-loading v-show="loading"></com-loading>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'App'
-}
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+   import Header from './components/header'
+   import Footer from './components/footer'
+   import Index from './page/index'
+   import loading from './components/loading.vue'
+   import addMsg from './page/addMsg.vue'
+
+  export default{
+    data: function () {
+      return {
+        transitionName: 'slide-left'
+      }
+    },
+    created: function () {
+      if (this.$route.name === undefined) {
+        //立即调整到 热门话题
+        this.$router.push('/index/hot')
+      }
+    },
+    watch: {
+      '$route' (to, from) {
+        const toDepth = to.path.split('/').length
+        const fromDepth = from.path.split('/').length
+        this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+      }
+    },
+    computed: {
+      isFooter: function () {
+        return this.$store.state.comm.indexConf.isFooter
+      },
+      loading: function () {
+        return this.$store.state.comm.loading
+      }
+    },
+    components: {
+      comHeader: Header,
+       comFooter: Footer,
+       comIndex: Index,
+       comLoading: loading,
+       comAddMsg:addMsg
+    },
+    methods: {}
+  }
+</script>
+<style lang="scss">
+  @import "../static/css/app.scss";
 </style>
--- vuedemo/src/main.js
+++ vuedemo/src/main.js
@@ -1,112 +1,33 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
-import VueResource from 'vue-resource'
-import axios from './ajax'
-Vue.prototype.$axios = axios
+import store from './store/store'
 
-Vue.config.productionTip = false
-Vue.use(VueResource)
-axios.interceptors.request.use(function(request){
-  console.log("axios request init")
-  return request
-})
-axios.interceptors.response.use(function(response){
-  console.log("axios request resp")
-  return response
-})
-// 超时设置
+//开启debug模式
+Vue.config.debug = true
+window.log = console.log
 
+let data = {
+  el:'#app',
+  router,
+  store
+}
+//创建一个app实例，并且挂载到选择符#app匹配的元素上
+new Vue(data).$mount('#app')
 
-/* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   router,
-//   components: { App },
-//   template: '<App/>'
-// })
-
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  },
-  created: function () {
-    // `this` 指向 vm 实例
-    console.log('a is: ' + this.message)
-    this.$axios.get('static/china.json').then((response) => {
-      console.log(response.data); this.data = response.data
-    }, (response) => { // error
-      console.log(response); console.log('axios err')
-    })
-  
-
-    // this.$http.get('static/china.json').then(res => {
-    //   console.log(res.body)
-    //   console.log("json数据为:" + res.body)//此处的res对象包含了json的文件信息和数据，我们需要的json数据存在于body属性中
-    // })
-  },
-  mounted:function(){
-  
-},
-})
-
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: '页面加载于 ' + new Date().toLocaleString()
-  }
-})
-
-var app3=new Vue({
-  el:'#app-3',
-  data:{
-    seen:true
-  }
-})
-
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: '学习 JavaScript' },
-      { text: '学习 Vue' },
-      { text: '创建激动人心的代码' }
-    ]
-  }
-})
-
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods:{
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
+Vue.prototype.post = function (url,params,fn) {
+  var xhr = new XMLHttpRequest()
+  xhr.open("POST", url, true)
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    var XMLHttpReq = xhr;
+    if (XMLHttpReq.readyState == 4) {
+      if (XMLHttpReq.status == 200) {
+        var text = XMLHttpReq.responseText
+        var res = JSON.parse(text)
+        fn(res)
+      }
     }
   }
-  
-})
+  xhr.send(params)
+}
 
-// 定义一个被命名为 todo-item 的新组件
-Vue.component('todo-item', {
-  // todo-item 组件现在接受一个 "prop"，
-  // 类似于一个自定义属性。
-  // 此 prop 名为 todo。
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-})
-
-var app7 = new Vue({
-  el: '#app-7',
-  data: {
-    groceryList: [
-      { id: 0, text: '蔬菜' },
-      { id: 1, text: '奶酪' },
-      { id: 2, text: '其他人类食物' }
-    ]
-  }
-})
