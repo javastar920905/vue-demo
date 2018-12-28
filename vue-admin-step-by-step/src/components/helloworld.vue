f52fd0f48d0f5c582a4654cfcde025ca173af72b
ouzx@hua-cloud.com.cn <ouzx@hua-cloud.com.cn>
2018-12-28 11:54:48 +0800

    补充vue-admin-step-by-step分支代码

---
+++ vue-admin-step-by-step/src/components/helloworld.vue
@@ -0,0 +1,146 @@
+<template>
+    <div id="helloworld">
+        <el-row>
+        <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+        </el-row>
+        <el-row>
+        <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+        </el-row>
+        <el-row>
+        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        </el-row>
+        <el-row>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        </el-row>
+
+
+        <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+        </el-row>
+
+        <el-row>
+        <el-button plain>朴素按钮</el-button>
+        <el-button type="primary" plain>主要按钮</el-button>
+        <el-button type="success" plain>成功按钮</el-button>
+        <el-button type="info" plain>信息按钮</el-button>
+        <el-button type="warning" plain>警告按钮</el-button>
+        <el-button type="danger" plain>危险按钮</el-button>
+        </el-row>
+
+        <el-row>
+        <el-button round>圆角按钮</el-button>
+        <el-button type="primary" round>主要按钮</el-button>
+        <el-button type="success" round>成功按钮</el-button>
+        <el-button type="info" round>信息按钮</el-button>
+        <el-button type="warning" round>警告按钮</el-button>
+        <el-button type="danger" round>危险按钮</el-button>
+        </el-row>
+
+        <el-row>
+        <el-button icon="el-icon-search" circle></el-button>
+        <el-button type="primary" icon="el-icon-edit" circle></el-button>
+        <el-button type="success" icon="el-icon-check" circle></el-button>
+        <el-button type="info" icon="el-icon-message" circle></el-button>
+        <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+        <el-button type="danger" icon="el-icon-delete" circle></el-button>
+        </el-row>
+
+<light></light>
+
+
+        
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+//引入 element-ui
+import {Carousel,CarouselItem} from 'element-ui';
+//el样式
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+
+Vue.component('light',{
+   render() {
+    return (
+        <div className="demo-3 medium">
+        <Carousel  arrow="always">
+                <Carousel.Item key="1"> <h3>1</h3></Carousel.Item>
+                <Carousel.Item key="2"> <h3>2</h3></Carousel.Item>
+                <Carousel.Item key="3"> <h3>3</h3></Carousel.Item>
+        </Carousel>
+        </div>
+    )
+    }
+})
+
+export default {
+    name:'helloworld',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App',
+             visible: false 
+        }
+    },
+    // data:function(){
+    //     return{ visible: false} 
+    // },
+    mounted() {
+       
+    },
+    methods: {
+
+    }
+
+}
+</script>
+<!--使用less-->
+<style lang="less">
+    .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
+
