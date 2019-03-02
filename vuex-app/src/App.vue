126633663d51d3c32ed630461e98bdec458b0673
javastar920905 <javastar920905@163.com>
2019-03-02 20:26:09 +0800

    页面布局和属性传值

---
+++ vuex-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!--父组件传值 -->
+    <ProductListOne v-bind:products="products"></ProductListOne>
+    <product-list-two v-bind:products="products"></product-list-two>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import ProductListOne from '@/components/ProductListOne';
+import ProductListTwo from '@/components/ProductListTwo';
+
+export default {
+  name: 'App',
+  components:{
+    ProductListOne,ProductListTwo
+  },
+  data(){
+    return {
+      products:[
+        {name:"马云",price:200},
+        {name:"马化腾",price:140},
+        {name:"马冬梅",price:20},
+        {name:"马荣",price:10}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuex-app/src/components/ProductListTwo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="product-list-two">
+    <h2>product list two</h2>
+    
+     <ul>
+      <li v-for="p in products" v-key="p.name">
+        <span class="name">{{p.name}}</span>
+         <span class="price">{{p.price}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'product-list-two',
+  // 接收属性传值
+  props:["products"],
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+#product-list-two{
+  background: #D1E4FF;
+  box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+  margin-bottom:30px;
+  padding: 10px 20px; 
+}
+#product-list-two ul{
+  list-style-type: none;
+  padding:0; 
+}
+#product-list-two li{
+ margin: 10px;
+ padding: 20px;
+ background: rgba(255, 255, 255, 0.7)
+}
+.price{
+  font-weight: bold;
+  color: orange;
+  display: block;
+}
+</style>
