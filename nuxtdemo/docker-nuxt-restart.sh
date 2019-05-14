2759a6599385a7f5665751679eb51743b065e643
javastar920905 <javastar920905@163.com>
2019-05-14 20:20:31 +0800

    修复服务无法访问问题

--- nuxtdemo/docker-nuxt-restart.sh
+++ nuxtdemo/docker-nuxt-restart.sh
@@ -8,6 +8,6 @@ serviceport=8000
 docker rm -f ${service}
 # docker images | grep none | awk '{print $3}' | xargs docker rmi -f
 # -p linuxport:docker port  nouseful when --net="host"
-docker run -d -m 1G --name  ${service} --net="host" -p ${serviceport}:8000 -t ${image}
+docker run -m 1G --name  ${service} --net="host" -t ${image}
 # docker logs -f --tail 1000  ${service}
 
