0d3563968703cffd81ab8ee66b89d239967c0aec
zyan <javastar920905@163.com>
2019-07-08 18:36:08 +0800

    文件上传测试

--- axios-test/src/App.vue
+++ axios-test/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png">
-    <HelloWorld/> -->
-    <h3>企业名称: {{mandatorName}}</h3>
+    <!-- <img src="./assets/logo.png"> -->
+    <HelloWorld/>
+    <!-- <h3>企业名称: {{mandatorName}}</h3>
     <p>信用代码: {{socialCreditCode}}</p>
     <p>未申报信息: </p>
     <textarea v-model="declares" name="" id="" cols="30" rows="10">
     
-    </textarea>
+    </textarea> -->
     
   </div>
 </template>
@@ -35,58 +35,58 @@ export default {
     //proxy table 没成功-404 api/declare/xgm/declare/taxDeclareList?busiType=unDeclare&skssqq=2018-07-01&skssqz=2018-09-30&pageNum=1&pageSize=10
     // nginx 配置允许跨域'http://localhost/tax-declare/xgm/declare/taxDeclareList?busiType=unDeclare&skssqq=2018-07-01&skssqz=2018-09-30&pageNum=1&pageSize=10'
     
-    let urlprefix='http://localhost/tax-declare/xgm/declare';
-    // let urlprefix='http://dev.7easytax.com/tax-declare/xgm/declare';
+    // let urlprefix='http://localhost/tax-declare/xgm/declare';
+    // // let urlprefix='http://dev.7easytax.com/tax-declare/xgm/declare';
 
-    let that=this;
-     console.log("发送get 请求")
-    this.$axios.get(urlprefix+'/taxDeclareList?busiType=unDeclare&skssqq=2018-07-01&skssqz=2018-09-30&pageNum=1&pageSize=10')
-    .then(function (response) {
-     // console.log(response.data.data);
-      //console.log(response.data.data.declares);
-      let declares=response.data.data.declares;
-     // console.log(declares);
-     // console.log(JSON.parse(response.data.data));
-      that.declares=declares;
-      let declare=that.declares[0];
-      that.socialCreditCode=declare.socialCreditCode;
-      that.goodsZzsXgmModelVO=declare.goodsZzsXgmModelVO;
-      that.goodsZzsXgmModelVO.socialCreditCode=that.socialCreditCode;
-      that.serviceZzsXgmModelVO=declare.serviceZzsXgmModelVO;
-      that.serviceZzsXgmModelVO.socialCreditCode=that.socialCreditCode;
-      //console.log(that.goodsZzsXgmModelVO);
-      console.log("发送get 请求完成")
+    // let that=this;
+    //  console.log("发送get 请求")
+    // this.$axios.get(urlprefix+'/taxDeclareList?busiType=unDeclare&skssqq=2018-07-01&skssqz=2018-09-30&pageNum=1&pageSize=10')
+    // .then(function (response) {
+    //  // console.log(response.data.data);
+    //   //console.log(response.data.data.declares);
+    //   let declares=response.data.data.declares;
+    //  // console.log(declares);
+    //  // console.log(JSON.parse(response.data.data));
+    //   that.declares=declares;
+    //   let declare=that.declares[0];
+    //   that.socialCreditCode=declare.socialCreditCode;
+    //   that.goodsZzsXgmModelVO=declare.goodsZzsXgmModelVO;
+    //   that.goodsZzsXgmModelVO.socialCreditCode=that.socialCreditCode;
+    //   that.serviceZzsXgmModelVO=declare.serviceZzsXgmModelVO;
+    //   that.serviceZzsXgmModelVO.socialCreditCode=that.socialCreditCode;
+    //   //console.log(that.goodsZzsXgmModelVO);
+    //   console.log("发送get 请求完成")
 
-        let updateZbUrl=urlprefix+'/declare/xgm/updateXgmZbOnListPage';//修改主表记录url
-        let zeroDeclareUrl=urlprefix+'/lxlsb/xgm/yjlsb';//零申报url
+    //     let updateZbUrl=urlprefix+'/declare/xgm/updateXgmZbOnListPage';//修改主表记录url
+    //     let zeroDeclareUrl=urlprefix+'/lxlsb/xgm/yjlsb';//零申报url
 
-        //构建主表数据
-        let ZzsXgmModelVO=[];
-        ZzsXgmModelVO.push(declare.goodsZzsXgmModelVO);
-        ZzsXgmModelVO.push(declare.serviceZzsXgmModelVO);
-         console.log("构建主表数据")
-        console.log(ZzsXgmModelVO)
+    //     //构建主表数据
+    //     let ZzsXgmModelVO=[];
+    //     ZzsXgmModelVO.push(declare.goodsZzsXgmModelVO);
+    //     ZzsXgmModelVO.push(declare.serviceZzsXgmModelVO);
+    //      console.log("构建主表数据")
+    //     console.log(ZzsXgmModelVO)
         
-        //构建零申报数据
-        let zeroDeclareVO=[];
-        declare.yjlsbVo.socialCreditCode=that.socialCreditCode;
-        zeroDeclareVO.push(declare.yjlsbVo);
-        console.log("构建零申报数据")
-        console.log(declare.yjlsbVo)
+    //     //构建零申报数据
+    //     let zeroDeclareVO=[];
+    //     declare.yjlsbVo.socialCreditCode=that.socialCreditCode;
+    //     zeroDeclareVO.push(declare.yjlsbVo);
+    //     console.log("构建零申报数据")
+    //     console.log(declare.yjlsbVo)
 
-        //发送post请求
-        console.log("发送post请求")
-        that.$axios.post(zeroDeclareUrl, zeroDeclareVO)
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-          })
-    .catch(function (error) {
-      console.log(error);
-    });
+    //     //发送post请求
+    //     console.log("发送post请求")
+    //     that.$axios.post(zeroDeclareUrl, zeroDeclareVO)
+    //       .then(function (response) {
+    //         console.log(response);
+    //       })
+    //       .catch(function (error) {
+    //         console.log(error);
+    //       });
+    //       })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
 
    
   }
--- axios-test/src/components/HelloWorld.vue
+++ axios-test/src/components/HelloWorld.vue
@@ -1,86 +1,22 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <div id="app" class="m-5">
+        <div class="uploadBox">
+            <h3>上传文件</h3>
+            <div class="fileBox">
+                <input type="file" id="myFile" class="inputfile" @change="handlerUpload($event)">
+                <label for="myFile">
+                    <i class="iconfont">&#xe632;</i>点击上传本地文件
+                </label>
+            </div>
+            <div class="fileInfo">
+                <ul class="files">
+                    <li  v-for="(file, index) in files" v-bind:key="file">
+                        {{ file.name }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -90,6 +26,33 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+   methods:{
+            handlerUpload(e){
+              let param = new FormData();
+              param.append("name", "wiiiiiinney");
+              //通过append向form对象添加数据
+              param.append("file", e.target.files[0]);
+              //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+              console.log(param.get("file"));
+
+                  let config = {
+                    //添加请求头
+                    headers: { "Content-Type": "multipart/form-data" },
+                    //添加上传进度监听事件
+                    onUploadProgress: e => {
+                      var completeProgress = ((e.loaded / e.total * 100) | 0) + "%";
+                      this.progress = completeProgress;
+                    }
+                  };
+
+                  this.$axios.post('http://seal:8083/upload', param, config).then(
+                  function (response) { 
+                    console.log(response); 
+                  }).catch(function (error) {
+                      console.log(error);
+                  });
+            }
   }
 }
 </script>
@@ -110,4 +73,13 @@ li {
 a {
   color: #42b983;
 }
+
+  .inputfile {
+            width: 0.1px;
+            height: 0.1px;
+            opacity: 0;
+            overflow: hidden;
+            position: absolute;
+            z-index: -1;
+        }
 </style>
